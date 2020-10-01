12ef5ac090364f559a0f0247fc91dd712afc2774
Erica Girges <ericafiorenza@gmail.com>
2020-09-30 20:18:11 -0700

    DELETE completed

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -34,8 +34,23 @@ router.get("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // do your magic!
+  Posts.remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "The post has been deleted." });
+      } else {
+        res.status(404).json({ message: "The post could not be found" });
+      }
+    })
+    .catch((error) => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the post",
+      });
+    });
 });
+
 
 router.put("/:id", (req, res) => {
   // do your magic!
