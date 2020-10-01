5ebbce5cd7d996c691834655381da95991882816
Erica Girges <ericafiorenza@gmail.com>
2020-09-30 19:58:35 -0700

    logger middleware & post GET endpoints completed

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,20 +1,43 @@
-const express = require('express');
+const express = require("express");
+
+const Posts = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  Posts.get(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving posts",
+      });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Posts.getById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Error retrieving post",
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const morgan = require("morgan");
+const express = require("express");
+const postRouter = require("./posts/postRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+server.use("/api/posts", postRouter);
 
 //custom middleware
 
-function logger(req, res, next) {}
+// logger middlerware
+function logger(req, res, next) {
+  const timestamp = new Date(Date.now()).toLocaleString();
+  console.log(`${req.method} ${req.url} ${timestamp}`);
+  next();
+};
+
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
